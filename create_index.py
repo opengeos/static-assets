d91128ca304d2738f063c236135cbbbcf1269b2a
Qiusheng Wu <giswqs@gmail.com>
2024-07-06 11:25:39 -0400

    Update directories

--- create_index.py
+++ create_index.py
@@ -44,7 +44,7 @@ def generate_index_html(directories, output_file):
 
 
 # Specify the directories to list
-directories_to_list = ["files"]
+directories_to_list = ["images", "vector"]
 # Specify the output HTML file
 output_html_file = "index.html"
 
